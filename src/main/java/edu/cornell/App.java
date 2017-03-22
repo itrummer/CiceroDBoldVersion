64ffb969ff32e37467ea08565a3ba76fd6a02a53
Mark Bryan <mab539@cornell.edu>
2017-03-21 21:55:49 -0400

    define some user commands

--- src/main/java/edu/cornell/App.java
+++ src/main/java/edu/cornell/App.java
@@ -12,8 +12,11 @@ import java.io.InputStream;
 import java.util.Scanner;
 
 
-public class App 
+public class App
 {
+    public static String QUIT_COMMAND = "\\q";
+    public static String HELP_COMMAND = "\\h";
+
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
@@ -24,6 +27,16 @@ public class App
             System.out.print("audiolizer> ");
             String input = scanner.nextLine();
             if (input.length() == 0) {
+                continue;
+            }
+
+            // define some special commands and check if user has input one of them
+            if (input.equals(QUIT_COMMAND)) {
+                scanner.close();
+                System.out.println("Bye");
+                System.exit(0);
+            } else if (input.equals(HELP_COMMAND)) {
+                System.out.println("Help: Insert help information here...");
                 continue;
             }
 
