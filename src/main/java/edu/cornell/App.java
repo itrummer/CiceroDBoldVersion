075d3f46d859bea117b78953d3cd0a4dea03e45c
Mark Bryan <mab539@cornell.edu>
2017-03-21 21:48:18 -0400

    play wav input stream from watson

--- src/main/java/edu/cornell/App.java
+++ src/main/java/edu/cornell/App.java
@@ -5,12 +5,10 @@ import com.ibm.watson.developer_cloud.text_to_speech.v1.model.AudioFormat;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.model.Voice;
 import com.ibm.watson.developer_cloud.text_to_speech.v1.util.WaveUtils;
 
+import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.Clip;
-import javax.sound.sampled.DataLine;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.util.Scanner;
 
 
@@ -19,6 +17,8 @@ public class App
     public static void main( String[] args )
     {
         Scanner scanner = new Scanner(System.in);
+        TextToSpeech service = new TextToSpeech();
+        service.setUsernameAndPassword("896e2e12-6a72-4ad9-908b-13d0ea6adc67", "ofpV28BqPARy");
 
         while(true) {
             System.out.print("audiolizer> ");
@@ -27,19 +27,14 @@ public class App
                 continue;
             }
 
-            TextToSpeech service = new TextToSpeech();
-            service.setUsernameAndPassword("896e2e12-6a72-4ad9-908b-13d0ea6adc67", "ofpV28BqPARy");
-
             try {
                 InputStream stream = service.synthesize(input, Voice.EN_ALLISON, AudioFormat.WAV).execute();
                 InputStream in = WaveUtils.reWriteWaveHeader(stream);
-                OutputStream out = new FileOutputStream("/Users/mabryan/Desktop/output.wav");
-                byte[] buffer = new byte[1024];
-                int length;
-                while ((length = in.read(buffer)) > 0) {
-                    out.write(buffer, 0, length);
-                }
-                out.close();
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(in);
+                Clip clip = AudioSystem.getClip();
+                clip.open(audioStream);
+                clip.start();
+                audioStream.close();
                 in.close();
                 stream.close();
             }
