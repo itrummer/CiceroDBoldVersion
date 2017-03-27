9ee32ec3297261ec104e550e514d3d6c98a4a330
Mark Bryan <mab539@cornell.edu>
2017-03-27 13:57:31 -0400

    method stub for matching tuples to contexts

--- src/main/java/planner/Context.java
+++ src/main/java/planner/Context.java
@@ -1,7 +1,19 @@
 package planner;
 
+import db.Tuple;
+
 /**
  * A set of value assignments
  */
 public class Context {
+
+    /**
+     * Determines if a given tuple matches this context. A Tuple matches a Context if it has the same ValueAssignments
+     * for all ValueAssignments in this Context.
+     * @param tuple The Tuple to check for a match
+     * @return True if tuple matches this context, else false.
+     */
+    public boolean match(Tuple tuple) {
+        return false;
+    }
 }
