a476b65a9de103183932cb1cda73cd023a72ab18
Mark Bryan <mab539@cornell.edu>
2017-04-03 19:20:04 +1000

    setup for cplex

--- src/main/java/planner/IntegerProgrammingPlanner.java
+++ src/main/java/planner/IntegerProgrammingPlanner.java
@@ -1,6 +1,8 @@
 package planner;
 
 import db.TupleCollection;
+import ilog.concert.*;
+import ilog.cplex.*;
 
 /**
  * This class constructs VoiceOutputPlans according to the integer programming model. It specifically uses the CPLEX
@@ -9,13 +11,26 @@ import db.TupleCollection;
 public class IntegerProgrammingPlanner extends VoicePlanner {
 
     /**
-     * Constructs a VoiceOutputPlan using the CPLEX integer programming solver.
+     * Constructs a VoiceOutputPlan using the CPLEX integer programming solver. Below are the
+     * variables and constraints that model the plan search space.
+     *
+     * Let n = number of Tuples in tupleCollection
+     *
+     * Maximum number of context "slots."
+     * cMax = floor(n/2)
+     *
      * @param tupleCollection The collection of Tuples to construct a voice plan
      * @return The optimal VoiceOutputPlan according to the integer programming approach
      */
     @Override
     public VoiceOutputPlan plan(TupleCollection tupleCollection) {
-//        TODO: initialize CPLEX instance and set constraints
+        try {
+            IloCplex cplex = new IloCplex();
+            // TODO: initialize integer programming model and solve it
+        } catch (IloException e) {
+            e.printStackTrace();
+        }
+
         return null;
     }
 }
