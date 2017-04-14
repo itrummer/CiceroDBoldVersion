69700b00da98aca067f82b78952fd5e83c511dde
Mark Bryan <mab539@cornell.edu>
2017-04-14 18:45:11 -0400

    it works!

--- src/main/java/planner/IntegerProgrammingPlanner.java
+++ src/main/java/planner/IntegerProgrammingPlanner.java
@@ -38,13 +38,15 @@ public class IntegerProgrammingPlanner extends VoicePlanner {
             IloCplex cplex = new IloCplex();
 
             // INITIALIZE INTEGER PROGRAMMING VARIABLE MATRICES
+            int[] categoricalLengths = tupleCollection.getLengthsOfCategoricalAttributes();
+            int[] numericalLengths = tupleCollection.getLengthsOfNumericalAttributes();
 
             IloIntVar[][] w = initialize2DCPLEXMatrix(cplex, cMax, tupleCount);
             IloIntVar[][] f = initialize2DCPLEXMatrix(cplex, cMax, attributeCount);
-            IloIntVar[][][] l = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, tupleCollection.getLengthsOfNumericalAttributes());
-            IloIntVar[][][] u = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, tupleCollection.getLengthsOfNumericalAttributes());
-            IloIntVar[][][] d = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, tupleCollection.getLengthsOfCategoricalAttributes());
-            IloIntVar[][][] e = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, tupleCollection.getLengthsOfNumericalAttributes());
+            IloIntVar[][][] l = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, numericalLengths);
+            IloIntVar[][][] u = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, numericalLengths);
+            IloIntVar[][][] d = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, categoricalLengths);
+            IloIntVar[][][] e = initializeVariable3DCPLEXMatrix(cplex, cMax, attributeCount, numericalLengths);
             IloIntVar[][][] s = initializeFull3DCPLEXMatrix(cplex, cMax, tupleCount, attributeCount);
             IloIntVar[] g = cplex.intVarArray(cMax, 0,1);
 
@@ -118,28 +120,28 @@ public class IntegerProgrammingPlanner extends VoicePlanner {
                         for (int t = 0; t < tupleCollection.tupleCount(); t++) {
                             Value vT = tupleCollection.getValueForAttributeAndTuple(a, t);
                             int vTIndex = tupleCollection.getIndexOfDistinctValue(a, t);
-                            // (1 - d(c,a,v_r)) + w(c,r) + f(c,a) <= 2
-                            // ==> -d(c,a,v_r)  + w(c,r) + f(c,a) <= 1
-                            // ==>                w(c,r) + f(c,a) <= 1 + d(c,a,v)
+                            // only one of the following terms can hold:
+                            // (1 - d(c,a,v_t)) + w(c,t) + f(c,a) <= 2
                             cplex.addLe(cplex.sum(w[c][t], f[c][a]), cplex.sum(1, d[c][a][vTIndex]));
                         }
                     } else {
                         for (int t = 0; t < tupleCollection.tupleCount(); t++) {
-//                        Value vT = valueMatrix[t][a];
-//                        int vTIndex = indexMap.get(vT);
-//                        for (int otherV = 0; otherV < valueList.size(); otherV++) {
-//                            // for each of the other distinct values
-//                            Value v = valueList.get(otherV);
-//                            if (v.compareTo(vT) > 0) {
-//                                // v > vT
-//                                // l(c,a,v) + w(c,r) + f(c,a) <= 2
-//                                cplex.addLe(cplex.sum(l[c][a][otherV], w[c][t], f[c][a]), 2);
-//                            } else if (v.compareTo(vT) < 0) {
-//                                // v < VT
-//                                // u(c,a,v) + w(c,r) + f(c,a) <= 2
-//                                cplex.addLe(cplex.sum(u[c][a][otherV], w[c][t], f[c][a]), 2);
-//                            }
-//                        }
+                            Value vT = tupleCollection.getValueForAttributeAndTuple(a, t);
+                            int vTIndex = tupleCollection.getIndexOfDistinctValue(a, t);
+
+                            for (int v = 0; v < tupleCollection.distinctValueCountForAttribute(a); v++) {
+                                if (v != vTIndex) {
+                                    if (tupleCollection.getDistinctValue(a, v).compareTo(vT) > 0) {
+                                        // v > vT
+                                        // constraint: l(c,a,v) + w(c,r) + f(c,a) <= 2
+                                        cplex.addLe(cplex.sum(l[c][a][v], w[c][t], f[c][a]), 2);
+                                    } else if (tupleCollection.getDistinctValue(a, v).compareTo(vT) < 0) {
+                                        // v < vT
+                                        // constraint: u(c,a,v) + w(c,r) + f(c,a) <= 2
+                                        cplex.addLe(cplex.sum(u[c][a][v], w[c][t], f[c][a]), 2);
+                                    }
+                                }
+                            }
                         }
                     }
                 }
@@ -215,7 +217,7 @@ public class IntegerProgrammingPlanner extends VoicePlanner {
             // 2. iterate through categorical assignments, create categorical value assignments to the appropriate context
             for (int c = 0; c < cMax; c++) {
                 for (int a = 0; a < attributeCount; a++) {
-                    for (int v = 0; v < tupleCollection.distinctValueCountForAttribute(a); v++) {
+                    for (int v = 0; v < categoricalLengths[a]; v++) {
                         ArrayList<Value> valuesInDomain = new ArrayList<Value>();
                         if (cplex.getValue(d[c][a][v]) > 0.5) {
                             valuesInDomain.add(tupleCollection.getDistinctValue(a, v));
@@ -231,8 +233,17 @@ public class IntegerProgrammingPlanner extends VoicePlanner {
             // 3. iterate through lower and upper bounds, create numerical value assignments to the appropriate context
             for (int c = 0; c < cMax; c++) {
                 for (int a = 0; a < attributeCount; a++) {
-                    for (int v = 0; v < tupleCollection.distinctValueCountForAttribute(a); v++) {
-                        // TODO
+                    Value lowerBound = null;
+                    Value upperBound = null;
+                    for (int v = 0; v < numericalLengths[a]; v++) {
+                        if (cplex.getValue(l[c][a][v]) > 0.5) {
+                            lowerBound = tupleCollection.getDistinctValue(a, v);
+                        } if (cplex.getValue(u[c][a][v]) > 0.5) {
+                            upperBound = tupleCollection.getDistinctValue(a, v);
+                        }
+                    }
+                    if (lowerBound != null && upperBound != null) {
+                        scopes.get(c).getContext().addNumericalValueAssignment(tupleCollection.attributeForIndex(a), lowerBound, upperBound);
                     }
                 }
             }
@@ -272,7 +283,7 @@ public class IntegerProgrammingPlanner extends VoicePlanner {
     }
 
     private IloIntVar[][] initialize2DCPLEXMatrix(IloCplex cplex, int rows, int columns) throws IloException {
-        IloIntVar[][] m = new IloIntVar[rows][];
+        IloIntVar[][] m = new IloIntVar[rows][0];
         for (int r = 0; r < rows; r++) {
             m[r] = cplex.intVarArray(columns, 0, 1);
         }
@@ -291,9 +302,7 @@ public class IntegerProgrammingPlanner extends VoicePlanner {
         IloIntVar[][][] m = new IloIntVar[contextCount][attributeCount][0];
         for (int c = 0; c < contextCount; c++) {
             for (int a = 0; a < attributeCount; a++) {
-                if (lengths[a] > 0) {
-                    m[c][a] = cplex.intVarArray(lengths[a], 0, 1);
-                }
+                m[c][a] = cplex.intVarArray(lengths[a], 0, 1);
             }
         }
         return m;
