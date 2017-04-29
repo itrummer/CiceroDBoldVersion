d28a33ce1ba9badf8c32e4dde16f29eb9bda7ab4
Mark Bryan <mab539@cornell.edu>
2017-04-29 16:46:12 -0400

    slowly building web app

--- src/main/java/web/HomeController.java
+++ src/main/java/web/HomeController.java
@@ -3,6 +3,7 @@ package web;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @SpringBootApplication
@@ -10,6 +11,11 @@ public class HomeController {
 
     public static void main(String[] args) throws Exception {
         SpringApplication.run(HomeController.class, args);
+    }
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
     }
 
 }
