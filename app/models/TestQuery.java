443366a3761eaabd7f8fb4906cfd87c777707226
Mark Bryan <mab539@cornell.edu>
2017-04-30 19:20:37 -0400

    basic functionality

---
+++ app/models/TestQuery.java
@@ -0,0 +1,19 @@
+package models;
+
+public class TestQuery {
+
+    String query;
+
+    public TestQuery() {
+
+    }
+
+    public void setQuery(String query) {
+        this.query = query;
+    }
+
+    public String getQuery() {
+        return query;
+    }
+
+}
