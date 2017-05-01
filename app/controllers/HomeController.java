f127ace8b24bb7c55ccf31018761d7e2defa3ddb
Mark Bryan <mab539@cornell.edu>
2017-05-01 12:23:37 -0400

    style changes

--- app/controllers/HomeController.java
+++ app/controllers/HomeController.java
@@ -10,14 +10,8 @@ import views.html.*;
  */
 public class HomeController extends Controller {
 
-    /**
-     * An action that renders an HTML page with a welcome message.
-     * The configuration in the <code>routes</code> file means that
-     * this method will be called when the application receives a
-     * <code>GET</code> request with a path of <code>/</code>.
-     */
     public Result index() {
-        return ok(index.render(""));
+        return ok(index.render());
     }
 
 }
