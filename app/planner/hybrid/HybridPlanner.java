d82abc20395ec42757895a6ef2a1e8a1221ae47b
Mark Bryan <mab539@cornell.edu>
2017-05-08 01:46:02 -0400

    translation to and from LP instance for hybrid planner

--- app/planner/hybrid/HybridPlanner.java
+++ app/planner/hybrid/HybridPlanner.java
@@ -1,16 +1,18 @@
 package planner.hybrid;
 
+import ilog.concert.IloException;
+import ilog.concert.IloIntVar;
+import ilog.concert.IloLinearIntExpr;
+import ilog.cplex.IloCplex;
 import planner.VoiceOutputPlan;
 import planner.VoicePlanner;
-import planner.elements.Context;
-import planner.elements.Tuple;
-import planner.elements.TupleCollection;
-import planner.elements.ValueDomain;
+import planner.elements.*;
 import util.DatabaseUtilities;
 
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 /**
  */
@@ -33,9 +35,90 @@ public class HybridPlanner extends VoicePlanner {
             System.out.println(c);
         }
 
+        VoiceOutputPlan plan = null;
 
+        try {
+            IloCplex cplex = new IloCplex();
 
-        return null;
+            // TODO: CPLEX variables
+
+            IloIntVar[][] w = new IloIntVar[1+contextCandidates.size()][];
+            for (int c = 0; c < w.length; c++) {
+                w[c] = cplex.intVarArray(tupleCollection.tupleCount(), 0, 1);
+            }
+
+            // constraint: each tuple mapped to one context
+            for (int t = 0; t < tupleCollection.tupleCount(); t++) {
+                IloLinearIntExpr sumForT = cplex.linearIntExpr();
+                for (int c = 0; c < w.length; c++) {
+                    sumForT.addTerm(1, w[c][t]);
+                }
+                cplex.addEq(sumForT, 1);
+            }
+
+            IloIntVar[] g = cplex.intVarArray(1 + contextCandidates.size(), 0, 1);
+            for (int c = 0; c < w.length; c++) {
+                // a context is used only if at least one tuple is output in it
+                cplex.addGe(g[c], cplex.sum(w[c]));
+            }
+
+            IloLinearIntExpr costExpr = cplex.linearIntExpr();
+            for (int c = 0; c < g.length-1; c++) {
+                Context context = contextCandidates.get(c);
+                costExpr.addTerm(context.toSpeechText(true).length(), g[c]);
+
+                for (int t = 0; t < tupleCollection.tupleCount(); t++) {
+                    costExpr.addTerm(tupleCollection.getTuples().get(t).toSpeechText(context, true).length(), w[c][t]);
+                }
+            }
+
+            // costs for empty context
+            for (int t = 0; t < tupleCollection.tupleCount(); t++) {
+                costExpr.addTerm(tupleCollection.getTuples().get(t).toSpeechText(true).length(), w[w.length-1][t]);
+            }
+
+            cplex.addMinimize(costExpr);
+
+            cplex.solve();
+
+            // parse CPLEX output
+
+            HashMap<Integer, ArrayList<Tuple>> tupleBins = new HashMap<>();
+
+            // create bins for tuples
+            for (int c = 0; c < g.length; c++) {
+                if (cplex.getValue(g[g.length-1]) > 0.5) {
+                    tupleBins.put(c, new ArrayList<>());
+                }
+            }
+
+            for (int c = 0; c < w.length; c++) {
+                for (int t = 0; t < w[c].length; t++) {
+                    if (cplex.getValue(w[c][t]) > 0.5) {
+                        tupleBins.get(c).add(tupleCollection.getTuples().get(t));
+                    }
+                }
+            }
+
+            ArrayList<Scope> scopes = new ArrayList<>();
+            if (tupleBins.containsKey(contextCandidates.size())) {
+                // add empty scope if tuples were mapped to it
+                scopes.add(new Scope(tupleBins.get(contextCandidates.size())));
+            }
+            Scope emptyScope;
+            for (int c = 0; c < contextCandidates.size(); c++) {
+                if (tupleBins.containsKey(c)) {
+                    scopes.add(new Scope(contextCandidates.get(c), tupleBins.get(c)));
+                }
+            }
+
+            plan = new VoiceOutputPlan(scopes);
+
+        } catch (IloException e) {
+            e.printStackTrace();
+        }
+
+        return plan;
     }
 
     public boolean useless(Context c, TupleCollection tupleCollection) {
