bda346703272ef6ba1dccccbd04feac0aee2a253
Mark Bryan <mab539@cornell.edu>
2017-07-09 22:20:10 -0700

    add JVM arg in maven

--- src/main/java/api/MainController.java
+++ src/main/java/api/MainController.java
@@ -1,9 +1,15 @@
 package api;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "CiceroDB";
+    }
 
 }
