cba2a487a075f1160c056faaa45105286b5b0d4a
Mark Bryan <mab539@cornell.edu>
2017-10-08 00:14:53 -0400

    move logic for building test instance into TestInstance

--- src/main/java/api/TestController.java
+++ src/main/java/api/TestController.java
@@ -1,16 +1,8 @@
 package api;
 
-import data.CSVConnector;
 import org.springframework.web.bind.annotation.*;
 import planning.PlanningManager;
 import planning.PlanningResult;
-import planning.VoicePlanner;
-import planning.elements.TupleCollection;
-import planning.planners.greedy.GreedyPlanner;
-import planning.planners.hybrid.HybridPlanner;
-import planning.planners.hybrid.TupleCoveringPruner;
-import planning.planners.linear.LinearProgrammingPlanner;
-import planning.planners.naive.NaiveVoicePlanner;
 
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -18,30 +10,10 @@ import java.util.concurrent.atomic.AtomicLong;
 public class TestController {
     private final AtomicLong counter = new AtomicLong();
     private final PlanningManager planningManager = new PlanningManager();
-    private final CSVConnector csvConnector = new CSVConnector();
 
     @RequestMapping(value = "/test", method = RequestMethod.POST)
     public TestResult newTest(@RequestBody TestInstance testInstance) throws Exception {
-        VoicePlanner planner;
-        switch (testInstance.algorithm) {
-            case "naive":
-                planner = new NaiveVoicePlanner();
-                break;
-            case "hybrid":
-                planner = new HybridPlanner(new TupleCoveringPruner(10));
-                break;
-            case "fantom-greedy":
-                planner = new GreedyPlanner();
-                break;
-            case "linear":
-                planner = new LinearProgrammingPlanner();
-                break;
-            default:
-                throw new Exception("Invalid/unsupported algorithm");
-        }
-
-        TupleCollection tupleCollection = csvConnector.buildTupleCollectionFromCSV(testInstance.getTuplesClassName(), testInstance.csvBody, testInstance.csvHeader.split(","));
-        PlanningResult result = planningManager.buildPlan(planner, tupleCollection, testInstance.getConfig());
+        PlanningResult result = planningManager.buildPlan(testInstance.getPlanner(), testInstance.getData(), testInstance.getConfig());
         return new TestResult(counter.incrementAndGet(), result);
     }
 
--- src/main/java/api/TestInstance.java
+++ src/main/java/api/TestInstance.java
@@ -1,12 +1,34 @@
 package api;
 
+import data.CSVConnector;
+import data.SQLConnector;
+import planning.VoicePlanner;
 import planning.config.Config;
+import planning.elements.TupleCollection;
+import planning.planners.greedy.GreedyPlanner;
+import planning.planners.hybrid.HybridPlanner;
+import planning.planners.hybrid.TupleCoveringPruner;
+import planning.planners.linear.LinearProgrammingPlanner;
+import planning.planners.naive.NaiveVoicePlanner;
 
 public class TestInstance {
+    final static CSVConnector csvConnector = new CSVConnector();
+    final static SQLConnector sqlConnector = new SQLConnector();
+
+    String sqlQuery;
     String csvHeader;
     String csvBody;
     String algorithm;
     Config config;
+    String tuplesClassName;
+
+    public String getSqlQuery() {
+        return sqlQuery;
+    }
+
+    public void setSqlQuery(String sqlQuery) {
+        this.sqlQuery = sqlQuery;
+    }
 
     public String getTuplesClassName() {
         return tuplesClassName;
@@ -15,8 +37,6 @@ public class TestInstance {
     public void setTuplesClassName(String tuplesClassName) {
         this.tuplesClassName = tuplesClassName;
     }
-
-    String tuplesClassName;
 
     public String getCsvHeader() {
         return csvHeader;
@@ -48,5 +68,33 @@ public class TestInstance {
 
     public void setConfig(Config config) {
         this.config = config;
+    }
+
+    public TupleCollection getData() throws Exception {
+        if (sqlQuery != null) {
+            return sqlConnector.buildTupleCollectionFromQuery(sqlQuery, tuplesClassName);
+        } else {
+            return csvConnector.buildTupleCollectionFromCSV(tuplesClassName, csvBody, csvHeader);
+        }
+    }
+
+    public VoicePlanner getPlanner() throws Exception {
+        switch (algorithm) {
+            case "naive":
+                return new NaiveVoicePlanner();
+            case "hybrid":
+                return new HybridPlanner(new TupleCoveringPruner(10));
+            case "greedy":
+                return new GreedyPlanner();
+            case "linear":
+                return new LinearProgrammingPlanner();
+        }
+        throw new InvalidAlgorithmException(algorithm);
+    }
+
+    public class InvalidAlgorithmException extends Exception {
+        public InvalidAlgorithmException(String algorithm) {
+            super("Invalid algorithm: " + algorithm);
+        }
     }
 }
